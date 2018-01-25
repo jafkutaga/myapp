798312e1eff7d3b083d3d4b45d8ba33ecec6517a
jafkutaga <35534652+jafkutaga@users.noreply.github.com>
2018-01-25 23:02:25 +0000

    runs locally but not on heroku. attempt fix

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
+
 var app = express();
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
 
